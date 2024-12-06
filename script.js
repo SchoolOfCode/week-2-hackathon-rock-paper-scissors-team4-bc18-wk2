360af1c50ccde19ef7a3bc7e10b7fdc57914d0c8
pepland <136370802+pepland@users.noreply.github.com>
2024-12-06 11:57:03 +0000

    Tasks 5, 6, 7 and bonuses 7.1, 7.2 done. Code cleaned.

--- script.js
+++ script.js
@@ -1,34 +1,69 @@
-/*
-let computerMove = "paper";
-*/
-let playerMove = prompt("rock, paper, scissors?");
-const moves = ["rock", "paper", "scissors"];
-let computerMove = moves[Math.floor(Math.random() * moves.length)];
+let userName = prompt(`Hi, what's your name?
 
-function getWinner(playerMove, computerMove) { 
-    if (playerMove == computerMove) {
-        alert("0");
+    Your username:
+    - can be up to 10 characters long;
+    - should only start with letters, not numbers or symbols; 
+    - should be capitalised.`);
+
+function onlyLetters(userName) {
+    return Boolean(userName.match(/^[A-Za-z]/));
     }
-    else {
-        if (playerMove == "rock" && computerMove == "paper"){
-        alert("-1");
-    }
-    else if (playerMove == "rock" && computerMove == "scissors"){
-        alert("+1");
-    }
-    else if (playerMove == "paper" && computerMove == "rock"){
-        alert("+1");
-    }
-    else if (playerMove == "paper" && computerMove == "scissors"){
-        alert ("-1");
-    }
-    else if (playerMove == "scissors" && computerMove == "rock"){
-        alert("-1");
-    }
-    else if (playerMove == "scissors" && computerMove == "paper"){
-        alert("+1");
-    }
-    }  
+    
+function Capitalised(userName){
+    return Boolean(userName.match(/^[A-Z]/));
 }
 
+while ((userName.length > 10) || (onlyLetters(userName) == false) || ( Capitalised(userName) == false)) {
+    alert("Sorry, your name doesn't meet the criteria");
+    userName = prompt(`Hi, what's your name?
+
+        Your username:
+        - can be up to 10 characters long;
+        - should only start with letters, not numbers or symbols; 
+        - should be capitalised.`);
+        
+    if ((userName.length <=10) && (onlyLetters(userName) == true) && (Capitalised(userName) == true)) {
+        break;
+    }
+}
+
+let numberGames = 1;
+let wins = 0;
+let draws = 0;
+let losses = 0;
+let score = 0;
+let playAgain = true;
+    
+while (playAgain == true) {
+let playerMove = prompt(`Hi ${userName}! What's your move? Pick rock, paper or scissors`);
+const moves = ["rock", "paper", "scissors"];
+let computerMove = moves[Math.floor(Math.random() * moves.length)];
+    
+function getWinner(playerMove, computerMove) { 
+    if (playerMove == computerMove) {
+        alert(`The computer chose ${computerMove}. It's a draw! We'll add 0 to your score.`);
+        draws++;
+    }
+    else if ((playerMove == "rock" && computerMove == "paper") || (playerMove == "paper" && computerMove == "scissors") || (playerMove == "scissors" && computerMove == "rock")) {
+        alert(`The computer chose ${computerMove}. You lose! We'll remove 1 to your score.`);
+        losses++;
+        score--;
+        }
+    else {
+        alert(`The computer chose ${computerMove}. You win! We'll add 1 to your score.`);
+        wins++;
+        score++;
+    }
+}
+    
 getWinner (playerMove, computerMove);
+alert(`${userName}, you played ${numberGames} games. You won ${wins}, drew ${draws} and lost ${losses} games. Your score is ${score}.`)
+playAgain = confirm("Do you want to play again?");
+    
+if (playAgain == true) {
+    numberGames++;
+}
+else {
+    alert(`Game over ${userName}. Your final score is ${score}. You won ${wins}, drew ${draws} and lost ${losses} games.`);
+}
+}
